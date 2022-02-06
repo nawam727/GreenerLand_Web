529a99b7953afc8958717c62dd37ea3eeed9f613
Nawam Sahasra Mandara Denawakage <nawamsahasra27th@gmail.com>
2022-02-06 21:39:39 +0530

    Adding project files and Uncompleted home index.php

---
+++ Include/PHP/Header.php
@@ -0,0 +1,12 @@
+<div class="header">
+			<a href="">GREENER LAND</a>
+				<nav class="header-center">
+				<ul>
+					<li> <a href="">Home</a> </li>
+					<li> <a href="">Shop</a> </li>
+                    <li> <a href="">Blog</a> </li>
+					<li> <a href="">About Us</a> </li>
+					<li> <a href="">Contacts</a> </li>
+				</ul>
+				</nav>
+		</div>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="Include/CSS/Header.css">
+		<link rel="stylesheet" href="Include/CSS/Style.css">
+		<style>
+			.home-hero{
+				background-image: url("Images/tree2.jpg");
+				width: 100%;
+		
+	position:absolute;
+			
+}
+		</style>
+    </head>
+    <body>
+		<?php include 'Include/PHP/Header.php' ?>
+
+        <div class="home-hero">
+				<div class="home-hero-contain">
+					<div class="home-hero-contain-text">
+					<h2>WELCOME TO GREENER LAND </h2>
+					<p>We are Sri Lanka’s first online tree planting community-building
+						platform offering many value addition packages aligning to
+						Sustainability and working towards a sustainable future with
+						 clean air that will make way for a carbon-neutral nation.</p>
+						</div>
+						 <div class="hero-btn">
+							<a href="" class="plant">Plant A Tree</a>
+						 </div>
+				</div>
+			</div>
+        </div>
+</body>
+</html>
