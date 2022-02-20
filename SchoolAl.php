9df0c2194c455da03dd19715f2916dce67e56083
Nuthara <nutharayuthmini@gmail.com>
2022-02-20 23:56:25 +0530

    update SchoolAl page

--- SchoolAl.php
+++ SchoolAl.php
@@ -8,7 +8,16 @@
     </head>
     <body>
         <?php include 'Include/PHP/Header.php' ?>
-
+        <section class="hero">
+			<div class="container">
+				<div class="hero-contant-left">
+					<h1>Why is planting trees important?</h1>
+                    <p>Trees highly contribute to a clean environment as they improve air quality through the process of photosynthesis – producing oxygen and taking in carbon dioxide. It also filters air intercepting unhealthy particles by removing dusts and absorbing pollutants like carbon monoxide, sulfur dioxide and nitrogen dioxide and letting the rain wash them after.
+                    </p>
+				</div>
+			</div>
+		</section>
+        <br>
 
 
         <?php include 'Include/PHP/Footer.php' ?>
