0724d6554fd709f754a2184b8425486a61b726eb
Nuthara <nutharayuthmini@gmail.com>
2022-02-21 01:21:04 +0530

    modify images

--- SchoolAl.php
+++ SchoolAl.php
@@ -17,6 +17,17 @@
 				</div>
 			</div>
 		</section>
+        <section>
+        <div class="tree-powers">
+				<h2 class="headline">Trees have real super powers!</h2>
+				<p>When a seed of a plant or tree grow it makes the area around it greener. Also, it supports many life forms. Birds make their nests, many reptiles and animals live on it or near it.<br>
+
+                Besides, all these many beautiful flowers, food growing on it. Moreover, many parts of trees such as roots, leaves, stem, flower, seeds, are also edible. Most importantly they never ask anything in return for their services and the gifts they give. Trees also keep the balance in the ecosystem and ecology.<br>
+
+                To conclude, we can say that trees are very important and beneficial for every life form on earth. Without them, the survival of life on earth will become difficult and after some time every species starts to die because of lack of oxygen on the planet. So, to save our lives and to survive we have to learn the importance of trees and also have to teach our children the importance of trees.
+                </p>
+		</div>
+        </section>
         <br>
 
 
