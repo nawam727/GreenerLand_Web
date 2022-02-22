01cece93e4e15b870d93c90a4accd80442833588
NaveenSilva <naveensilva000@gmail.com>
2022-02-23 00:00:58 +0530

    modified cards using Javascript

--- About Us.php
+++ About Us.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="Include/CSS/About Us.css">
         <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
 		<link rel="stylesheet" href="Include/CSS/footer.css">
+		<link rel="stylesheet" href="Include/CSS/Animation.css">
     </head>
     <body>
         <?php include 'Include/PHP/Header.php' ?>
@@ -29,21 +30,21 @@
         </section>
 		<section>
 		<div class="vmp-con">
-				<div class="card3">
+				<div class="card3 animation-div card-first">
 					<div class="card3-container">
 						<h4>Mission</h4>
 						<i class="fi fi-rr-target"></i>
 						<p>Our mission is to support Sri Lanka's Green Vision and sustainability drive in order to create a sustainable environment, a sustainable future, and a carbon-neutral nation.</p>
 					</div>
 				</div>
-				<div class="card3">
+				<div class="card3 animation-div card-first">
 					<div class="card3-container">
 						<h4>Vision</h4>
 						<i class="fi fi-rr-globe-alt"></i>
 						<p>Empowering the youth of Sri Lanka towards a green and sustainable future by providing unparalleled services for sustainability.</p>
 					</div>
 				</div>
-				<div class="card3">
+				<div class="card3 animation-div card-first">
 					<div class="card3-container">
 						<h4>Philosophy</h4>
 						<i class="fi fi-rr-book-alt"></i>
@@ -83,7 +84,7 @@ Chairman – Breathe Environment & Sustainability (Pvt) Ltd</p>
 				<h2 class="headline"> Our Projects</h2>
 			</div>
 			<div class="our-project-contain">
-				<div class="card">
+				<div class="card animation-div card-first">
 					<div class="card-container">
 						<h4>Kaluthra Temple</h4>
 						<i class="fi fi-rr-user"></i>
@@ -91,7 +92,7 @@ Chairman – Breathe Environment & Sustainability (Pvt) Ltd</p>
 						<a href="About Us.php" class="Readmore">Read More</a>
 					</div>
 				</div>
-				<div class="card">
+				<div class="card animation-div card-first">
 					<div class="card-container">
 						<h4>Gangaramaya Temple</h4>
 						<i class="fi fi-rr-diploma"></i>
@@ -99,7 +100,7 @@ Chairman – Breathe Environment & Sustainability (Pvt) Ltd</p>
 						<a href="" class="Readmore">Read More</a>
 					</div>
 				</div>
-				<div class="card">
+				<div class="card animation-div card-first">
 					<div class="card-container">
 						<h4>Anuradapura Temple</h4>
 						<i class="fi fi-rr-tree"></i>
@@ -109,6 +110,7 @@ Chairman – Breathe Environment & Sustainability (Pvt) Ltd</p>
 				</div>	
 			<div>
 		</section>	
+		<script src="Include/JS/Animation.js"></script>
         <?php include 'Include/PHP/Footer.php' ?>
     </body>
 </html>
