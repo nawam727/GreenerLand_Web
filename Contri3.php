3c77e4eec211c112fb56c8676e9a5ba07db76560
Nedmi Vidusi Navanjali Abrew <96585093+nedmi16@users.noreply.github.com>
2022-02-22 20:43:17 +0530

    Modified files

--- Contri3.php
+++ Contri3.php
@@ -8,7 +8,22 @@
     </head>
     <body>
         <?php include 'Include/PHP/Header.php' ?>
+        <section class="hero">
+			<div class="container">
+				<div class="hero-contant-left">
+					<h2>BEACHES</h2>
+					<p></p>
+				</div>
+			</div>
+		</section>
+        <section>
 
+		<div class="tree-plant">
+				<h2 class="headline">OUR EFFORTS</h2>
+				<p>Among the projects we have initiated is a new project called Coastal Projects where we plant coastal mangroves around Sri Lanka such as coconut saplings to prevent seawater stagnant sea erosion and plant adaptation to the soil composition associated with it.We carried out projects on beaches that attracted tourists such as Bentota, Galle and Hikkaduwa.</P>
+			
+			</div>
+		</section>
         <?php include 'Include/PHP/Footer.php' ?>
     </body>
 </html>
